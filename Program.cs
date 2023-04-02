6fde5d712fa75faa0b2440d748da475745388bd5
Minsetr0 <nesterovmaksim71@mail.ru>
2023-04-02 22:27:43 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Random rand = new Random();
+int number = rand.Next(1, 28);
+int multiplicity = 0;
+
+for (int i = 100; i <= 999; i++)
+{
+	int i2 = i; 
+
+	while (i2 > 0)
+	{
+		i2 = i2 - number;
+	}
+
+	if (i2 == 0)
+	{
+		multiplicity++;
+    }
+}
+
+Console.WriteLine("Количество трёхзначных чисел, кратных " + number + ", равно " + multiplicity + ".");
